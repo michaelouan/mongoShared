6420335dbe8e210f262e58015bdb68c3812a7636
michaelouan <mich.ouanounou@gmail.com>
2022-02-07 15:35:57 +0200

    fix

--- workerThread.js
+++ workerThread.js
@@ -7,25 +7,25 @@ const sharedCollection = await getsharedCollection()
 const nonSharedCollection = await getNonsharedCollection()
 
 parentPort.on("message" , msg => {
-    benchmarkTest()
+    benchmarkTest(msg)
 })
 
-const benchmarkTest = async () => {
+const benchmarkTest = async (msg) => {
     console.log("run test on sharedColletion from thread")
     await runTestOnCollection(sharedCollection)
 
     console.log("run test on nonSharedColletion from thread")
     await runTestOnCollection(nonSharedCollection)
    
-    console.log("max insert in 5 second from thread on sharedCollection") 
-    await maxOneInsertPerSecond(sharedCollection).then((value) => {
-       console.log(value)
-    })
+    // console.log("max insert in 5 second from thread on sharedCollection") 
+    // await maxOneInsertPerSecond(sharedCollection).then((value) => {
+    //    console.log(value)
+    // })
 
-    console.log("max insert in 5 second from thread on nonSharedCollection") 
-    await maxOneInsertPerSecond(nonSharedCollection).then((value) => {
-       console.log(value)
-    })
+    // console.log("max insert in 5 second from thread on nonSharedCollection") 
+    // await maxOneInsertPerSecond(nonSharedCollection).then((value) => {
+    //    console.log(value)
+    // })
 
-   parentPort.postMessage({res : "opertations from thread success finished"})
+   parentPort.postMessage({res : `opertations from thread success finished after ${(Date.now() - msg)/1000 }`})
 }
